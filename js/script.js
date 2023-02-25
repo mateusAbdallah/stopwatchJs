424250d71cab3cee5e93c33663e3e94c63f6e44d
Mateus Abdallah <abdallahmateus@gmail.com>
2023-02-24 19:21:40 -0800

    getting elements in the html

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const minutesEl = document.querySelector("#minutes")
+const secondsEl = document.querySelector("#seconds")
+const milisecondsEl = document.querySelector("#miliseconds")
+const startBtn = document.querySelector("#startBtn")
+const pauseBtn = document.querySelector("#pauseBtn")
+const resumeBtn = document.querySelector("#resumeBtn")
+const resetBtn = document.querySelector("#resetBtn")
+
+let minutes = 0
+let seconds = 0
+let miliseconds = 0
+let isPaused = false
+
+
