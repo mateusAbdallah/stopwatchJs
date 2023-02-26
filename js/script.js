54724118a98447f497474c2c93838d3c4b3611c6
Mateus Abdallah <abdallahmateus@gmail.com>
2023-02-26 13:51:49 -0800

    setting function to the start button

--- js/script.js
+++ js/script.js
@@ -12,3 +12,26 @@ let miliseconds = 0
 let isPaused = false
 
 
+startBtn.addEventListener("click", startTime)
+
+function startTime () {
+    interval = setInterval(() => {
+        if(!isPaused){
+            miliseconds += 10;
+
+            if(miliseconds === 1000){
+                seconds++
+                miliseconds = 0
+            }
+
+            if(seconds === 60){
+                minutes++
+                seconds = 0
+            }
+
+            minutesEl.textContent = minutes
+            secondsEl.textContent = seconds
+            milisecondsEl.textContent = miliseconds
+        }
+    }, 10)
+}
